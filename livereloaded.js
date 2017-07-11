ab761b6081d9c7d4cc9b3e5e75d79de78121a0b9
Iivari Äikäs <iivari.aikas@gmail.com>
2017-07-11 21:54:50 +0300

    Re-inject livereload script after page reload

--- livereloaded.js
+++ livereloaded.js
@@ -1,8 +1,29 @@
+// State, contains tabs which have livereloading enabled.
+let enabledTabs = []
 
 const toolbarButtonHandler = tab => {
-  browser.tabs.executeScript({
+  enabledTabs.push(tab)
+  browser.tabs.executeScript(tab.id, {
+    file: 'livereload-script-tag-injector.js'
+  })
+}
+
+const tabRemovedHandler = id => {
+  const newState = enabledTabs.filter(et => et.id !== id)
+  enabledTabs = newState
+}
+
+const tabChangedHandler = (id, changedInfo) => {
+  const et = enabledTabs.find(et => et.id === id)
+  if (!et || changedInfo.status === 'loading') {
+    return
+  }
+
+  browser.tabs.executeScript(et.id, {
     file: 'livereload-script-tag-injector.js'
   })
 }
 
 browser.browserAction.onClicked.addListener(toolbarButtonHandler)
+browser.tabs.onRemoved.addListener(tabRemovedHandler)
+browser.tabs.onUpdated.addListener(tabChangedHandler)
