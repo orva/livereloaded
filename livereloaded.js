c90fbb0aab1f3f161549819ff4cb8f677657ddd6
Iivari Äikäs <iivari.aikas@gmail.com>
2017-07-12 12:35:50 +0300

    We can be toggled ON/OFF

--- livereloaded.js
+++ livereloaded.js
@@ -1,39 +1,54 @@
 // State, contains tabs which have livereloading enabled.
 let enabledTabs = []
 
-const injectScript = tabId =>
-  browser.tabs.executeScript(tabId, {
-    file: 'livereload-script-tag-injector.js'
-  })
-  .then(() => browser.tabs.sendMessage(tabId, { command: "inject" }))
+const removeTabFromState = id => {
+  // This should probably be redundant
+  browser.browserAction.setBadgeText({ text: "", tabId: id })
 
-const toolbarButtonHandler = tab => {
-  if (enabledTabs.some(t => t.id === tab.id)) {
-    return
-  }
-
-  enabledTabs.push(tab)
-
-  browser.browserAction.setBadgeText({ text: "ON", tabId: tab.id })
-  browser.browserAction.setBadgeBackgroundColor({ color: "#1496bb", tabId: tab.id })
-
-  return injectScript(tab.id)
-}
-
-const tabRemovedHandler = id => {
   const newState = enabledTabs.filter(et => et.id !== id)
   enabledTabs = newState
 }
 
+const storeTabToState = tab => {
+  if (!enabledTabs.some(t => t.id === tab.id)) {
+    enabledTabs.push(tab)
+  }
+}
+
+const injectScript = tab => {
+  storeTabToState(tab)
+  browser.browserAction.setBadgeText({ text: "ON", tabId: tab.id })
+  browser.browserAction.setBadgeBackgroundColor({ color: "#1496bb", tabId: tab.id })
+
+  return browser.tabs.executeScript(tab.id, {
+    file: 'livereload-script-tag-injector.js'
+  })
+  .then(() => browser.tabs.sendMessage(tab.id, { command: "inject" }))
+}
+
+const removeScript = tab => {
+  removeTabFromState(tab.id)
+  return browser.tabs.reload(tab.id)
+}
+
+const toggleLivereload = tab => {
+  if (enabledTabs.some(t => t.id === tab.id)) {
+    return removeScript(tab)
+  } else {
+    return injectScript(tab)
+  }
+}
+
+
 const tabChangedHandler = (id, changedInfo) => {
-  const et = enabledTabs.find(et => et.id === id)
-  if (!et || changedInfo.status === 'loading') {
+  const tab = enabledTabs.find(et => et.id === id)
+  if (!tab || changedInfo.status !== 'complete') {
     return
   }
 
-  return injectScript(et.id)
+  return injectScript(tab)
 }
 
-browser.browserAction.onClicked.addListener(toolbarButtonHandler)
-browser.tabs.onRemoved.addListener(tabRemovedHandler)
+browser.browserAction.onClicked.addListener(toggleLivereload)
+browser.tabs.onRemoved.addListener(removeTabFromState)
 browser.tabs.onUpdated.addListener(tabChangedHandler)
