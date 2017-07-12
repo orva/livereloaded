14b9530fc119337cca93fc0c16366f4aaec8c340
Iivari Äikäs <iivari.aikas@gmail.com>
2017-07-12 12:43:21 +0300

    Wrap injector script into closure

--- livereload-script-tag-injector.js
+++ livereload-script-tag-injector.js
@@ -1,17 +1,17 @@
-const injectLivereload = () => {
-  const script = document.createElement("script")
-  script.setAttribute("src", "http://localhost:35729/livereload.js")
-  document.head.appendChild(script)
-}
-
-browser.runtime.onMessage.addListener(msg => {
-  switch (msg.command) {
-    case "inject":
-      injectLivereload();
-      break;
-    default:
-      break;
+(() => {
+  const injectLivereload = () => {
+    const script = document.createElement("script")
+    script.setAttribute("src", "http://localhost:35729/livereload.js")
+    document.head.appendChild(script)
   }
-})
 
-true
+  browser.runtime.onMessage.addListener(msg => {
+    switch (msg.command) {
+      case "inject":
+        injectLivereload();
+        break;
+      default:
+        break;
+    }
+  })
+})()
