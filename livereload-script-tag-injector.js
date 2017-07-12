897357ded128cb0dd9f8b8169220bff702aa1fad
Iivari Äikäs <iivari.aikas@gmail.com>
2017-07-12 12:11:20 +0300

    Remove livereload script tag removing
    Removing the script tags does not remove the code from running, so this
    has no value.

--- livereload-script-tag-injector.js
+++ livereload-script-tag-injector.js
@@ -4,18 +4,10 @@ const injectLivereload = () => {
   document.head.appendChild(script)
 }
 
-const removeLivereload = () => {
-  const elems = document.querySelectorAll("script[src='http://localhost:35729/livereload.js']")
-  elems.forEach(s => s.remove())
-}
-
 browser.runtime.onMessage.addListener(msg => {
   switch (msg.command) {
     case "inject":
       injectLivereload();
-      break;
-    case "remove":
-      removeLivereload();
       break;
     default:
       break;
