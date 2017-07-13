79ce603f9b529e6a62be5c016415712c15f17a7b
Iivari Äikäs <iivari.aikas@gmail.com>
2017-07-13 18:02:09 +0300

    Remove closure wrapping around options.js

--- js/options.js
+++ js/options.js
@@ -1,45 +1,41 @@
-(() => {
-  const DEFAULT_PORT = 35729;
-  const fetchDefaultPort = () =>
-    browser.storage.sync
-      .get("defaultPort")
-      .then(d => d.defaultPort || DEFAULT_PORT);
-  const setDefaultPort = port => {
-    const defaultPort = port || DEFAULT_PORT;
-    return browser.storage.sync.set({ defaultPort }).then(() => defaultPort);
-  };
+const DEFAULT_PORT = 35729;
+const fetchDefaultPort = () =>
+  browser.storage.sync
+    .get("defaultPort")
+    .then(d => d.defaultPort || DEFAULT_PORT);
+const setDefaultPort = port => {
+  const defaultPort = port || DEFAULT_PORT;
+  return browser.storage.sync.set({ defaultPort }).then(() => defaultPort);
+};
 
-  const restoreFromStorege = () =>
-    fetchDefaultPort().then(port => {
-      const portInput = document.getElementsByClassName(
-        "default-port-input"
-      )[0];
-      portInput.value = port;
-    });
-
-  const bindListeners = () => {
+const restoreFromStorege = () =>
+  fetchDefaultPort().then(port => {
     const portInput = document.getElementsByClassName("default-port-input")[0];
-    portInput.addEventListener("input", e => {
-      const val = e.target.value;
+    portInput.value = port;
+  });
 
-      const containsNonDigits = /\D+/.test(val);
-      if (val === "" || containsNonDigits) {
-        portInput.classList.add("broken-input");
-        return;
-      }
+const bindListeners = () => {
+  const portInput = document.getElementsByClassName("default-port-input")[0];
+  portInput.addEventListener("input", e => {
+    const val = e.target.value;
 
-      portInput.classList.remove("broken-input");
-      const port = parseInt(val, 10);
-      return setDefaultPort(port);
-    });
+    const containsNonDigits = /\D+/.test(val);
+    if (val === "" || containsNonDigits) {
+      portInput.classList.add("broken-input");
+      return;
+    }
 
-    const reset = document.getElementsByClassName("reset-port-button")[0];
-    reset.addEventListener("click", () =>
-      setDefaultPort(DEFAULT_PORT).then(port => (portInput.value = port))
-    );
-  };
+    portInput.classList.remove("broken-input");
+    const port = parseInt(val, 10);
+    return setDefaultPort(port);
+  });
 
-  document.addEventListener("DOMContentLoaded", () =>
-    restoreFromStorege().then(() => bindListeners())
+  const reset = document.getElementsByClassName("reset-port-button")[0];
+  reset.addEventListener("click", () =>
+    setDefaultPort(DEFAULT_PORT).then(port => (portInput.value = port))
   );
-})();
+};
+
+document.addEventListener("DOMContentLoaded", () =>
+  restoreFromStorege().then(() => bindListeners())
+);
