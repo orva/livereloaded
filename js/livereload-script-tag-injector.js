31ea4607e1ab824db41a1f5bf1daf98dc8a4d97e
Iivari Äikäs <iivari.aikas@gmail.com>
2017-07-14 13:29:14 +0300

    Help user with only one console.error call

--- js/livereload-script-tag-injector.js
+++ js/livereload-script-tag-injector.js
@@ -14,10 +14,9 @@ const getHostname = () => {
 
 const reportCouldNotConnect = url => {
   /* eslint-disable no-console */
-  console.error("livereloaded: could not find livereload script at", url);
-  console.error(
-    "livereloaded: check that livereload server is running or set correct port in the addon preferences"
-  );
+  console.error(`livereloaded: could not find livereload script at ${url}
+- check that livereload server is running
+- check that livereload server port matches port in addon preferences`);
   /* eslint-enable no-console */
 };
 
